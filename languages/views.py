1ba289fb6d24abf2249b7c2504c203ea37904951
= <nitinuniyal21@gmail.com>
2018-10-01 00:39:18 +0530

    rest_framework based permissions added

--- languages/views.py
+++ languages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Language, Programmer, Paradigm
 from .serializers import LanguageSerializer, ProgrammerSerializer, ParadigmSerializer
 
@@ -7,6 +7,7 @@ from .serializers import LanguageSerializer, ProgrammerSerializer, ParadigmSeria
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class ParadigmView(viewsets.ModelViewSet):
     queryset = Paradigm.objects.all()
